8af47dc053e173300f443f6b627d5e1b80ac7066
Alberto Hitiro Tsugawa <hitiro@live.com>
2023-01-09 13:25:10 -0300

    feat: atualizando perfil e fazendo upload de imagem

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,7 @@ import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 import Dashboard from '../pages/Dashboard';
 import Profile from '../pages/Profile';
+import Customers from '../pages/Customers';
 
 
 export default function Routes() {
@@ -15,6 +16,7 @@ export default function Routes() {
 
       <Route exact path="/dashboard" component={Dashboard} isPrivate />
       <Route exact path="/profile" component={Profile} isPrivate />
+      <Route exact psth="/customers" component={Customers} isPrivate />
     </Switch>
   )
 }
